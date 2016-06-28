b259745374d11b2b03940ed3519a51d474edc441
shailesh108 <sp98484@gmail.com>
2016-06-28 16:03:37 +0530

    Rails-api

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,83 @@
+class TasksController < ApplicationController
+  include ActionController::MimeResponds
+  skip_before_filter :verify_authenticity_token
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
+
+  before_action :set_task, only: [:show, :update, :destroy]
+
+  # GET /tasks
+  # GET /tasks.json
+  def index
+    @tasks = Task.all
+
+    respond_to do |format|
+      format.json { render json: @tasks }
+      format.xml { render xml: @tasks }
+    end
+  end
+
+  # GET /tasks/1
+  # GET /tasks/1.json
+  def show
+    render json: @task
+  end
+
+  # POST /tasks
+  # POST /tasks.json
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      render json: @task, status: :created, location: @task
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /tasks/1
+  # PATCH/PUT /tasks/1.json
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      head :no_content
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /tasks/1
+  # DELETE /tasks/1.json
+  def destroy
+    @task.destroy
+
+    head :no_content
+  end
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  def cors_preflight_check
+    if request.method == 'OPTIONS'
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
+      headers['Access-Control-Max-Age'] = '1728000'
+
+      render :text => '', :content_type => 'text/plain'
+    end
+  end
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:name)
+  end
+end
